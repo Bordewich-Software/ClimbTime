4cbd59e143e9ec3929c372e50f691e32d4ac773c
Trond Bordewich <trondhb@gmail.com>
2023-06-12 20:20:03 +0200

    Add logos for tkk

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import {Typography} from "@mui/material";
+import {Grid, Typography} from "@mui/material";
+import Image from "next/image";
+import tkkWhite from "../public/TKKWhiteTransparent.svg";
 
 
 export default function Header() {
     return (
-        <Typography variant="body2" color="text.secondary" align="center">
-            {'The Climb Timer'}
+    <Grid container  alignItems={"center"} justifyContent={"space-between"}>
+        <Typography variant="h4" color="text.secondary" align="center">
+            {'Climb Timer'}
         </Typography>
+        <Image src={tkkWhite} alt={"itema-logo"} width={100}/>
+    </Grid>
     );
 }
